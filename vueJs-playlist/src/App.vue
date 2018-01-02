95b9403bb5fba8d69d4d4e51317df3befaf91b56
puribey <kenshipichu@gmail.com>
2018-01-02 18:23:38 -0300

    2-1-2018 new vue cli with webpack

---
+++ vueJs-playlist/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+
+<script>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import Ninjas from './Ninjas.vue'
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
+</style>
---
+++ vueJs-playlist/src/Ninjas.vue
@@ -0,0 +1,60 @@
+<template>
+  <section id="ninjas">
+    <ul>
+      <li v-for="ninja in ninjas" @click="ninja.show =! ninja.show">
+        <h2>{{ ninja.name }}</h2>
+        <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
+      </li>
+    </ul>
+  </section>
+</template>
+
+
+<script>
+export default {
+  components: {
+    
+  },
+  data() {
+    return {
+      ninjas: [
+        {name: 'Ryu', speciality: 'Vue Components', show: false},
+        {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+        {name: 'Hitoshi', speciality: 'Click Events', show: false},
+        {name: 'Tango', speciality: 'Conditionals', show: false},
+        {name: 'Kami', speciality: 'Webpack', show: false},
+        {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#ninjas{
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+ul{
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li{
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid purple;
+    box-shadow: 2px 2px 5px #ccc;
+    margin: 10px;
+}
+
+h3 {
+  color: purple;
+}
+</style>
